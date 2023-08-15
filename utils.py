a6355d214f57e2ccdcd2941a2e281bf8be9e07af
nourhan-waleeed <83437486+nourhan-waleeed@users.noreply.github.com>
2023-08-15 15:50:07 +0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 11:56:32 2023
+
+@author: nourhan
+"""
+import re
+
+
+columns = ['recency_in_days','days_since_joining','visit_frequency','avg_order','average_time_spent']
